d3a69a729af52587d10fb95b639d2d459d9dfef6
云墨安 <ymoan@qq.com>
2025-08-19 16:21:03 +0800

    🤣新增Group菜单
    嗯就是啥呢，阿就是增加一个管理功能菜单，给未来的群管之类的功能做准备

---
+++ config/help/Group.js
@@ -0,0 +1,55 @@
+export const helpCfg = {
+    "themeSet": false,
+    "title": "不羡仙管理帮助",
+    "subTitle": "Yunzai-Bot & BXX-Plugin",
+    "colWidth": 265,
+    "theme": "all",
+    "themeExclude": [
+        "default"
+    ],
+    "colCount": 3,
+    "bgBlur": true
+}
+
+export const helpList = [
+    {
+        "group": "群管功能",
+        "list": [
+            {
+                "icon": 81,
+                "title": "#禁言+Q号或@",
+                "desc": "禁言"
+            },
+        ]
+    },
+    {
+        "group": "好友功能",
+        "list": [
+            {
+                "icon": 12,
+                "title": "#发好友",
+                "desc": "给好友发送消息"
+            },
+        ]
+    },
+    {
+        "group": "QQ功能",
+        "list": [
+            {
+                "icon": 12,
+                "title": "#改名字",
+                "desc": "给QQ机器人改名"
+            },
+        ]
+    },
+    {
+        "group": "不羡仙功能管理",
+        "list": [
+            {
+                "icon": 58,
+                "title": "#不羡仙管理帮助",
+                "desc": "查看不羡仙管理帮助"
+            },
+        ]
+    }
+]
