f2f6e3931a7416bd98dee71d476baed29fb4d8db
云墨安 <ymoan@qq.com>
2025-08-23 15:48:06 +0800

    😍修复不羡仙更新问题

--- apps/set/更新.js
+++ apps/set/更新.js
@@ -11,25 +11,20 @@ export class Updates extends plugin {
             priority: -10,
             rule: [
                 {
-                    reg: /^#*(BXX|不羡仙)(插件)?(强制|強制)?更新$/i,
+                    reg: /^#*(不羡仙|BXX)(插件)?(强制|強制)?更新$/i,
                     fnc: 'update'
                 }
             ]
         })
-    }
-    
+    }   
     async update(e = this.e, isauto = false) {
         if (!e.isMaster) {
             await e.reply("暂无权限，只有主人才能操作");
             return true; 
         }
-        
         e.isMaster = true
-        
         if (e.at && !e.atme) return;
-        
-        e.msg = `#${e.msg.includes("强制") ? "强制" : ""}更新不羡仙`;
-
+        e.msg = `#${e.msg.includes("强制") ? "强制" : ""}更新BXX-plugin`;
         const up = new Update(e);
         up.e = e;
         return up.update();
