98df815b1a586e8de60fa7679e585e4dee2d5da2
云墨安 <ymoan@qq.com>
2025-08-26 12:08:59 +0800

    😘修改图像安装/更新源为【gitcode】

--- apps/instrument/图像更新.js
+++ apps/instrument/图像更新.js
@@ -20,8 +20,8 @@ export class ImageUpdate extends plugin {
                 }
             ]
         })
-        this.repoUrl = 'https://github.com/ymoan/normal-character.git'// 装不上请在https前加“https://ghfast.top/”
-        // this.repoUrl = 'https://gitcode.com/ymoan/normal-character.git'国内gitcode源可替换
+        this.repoUrl = 'https://gitcode.com/ymoan/normal-character.git' //gitcode源加速下载（可更换为github源库）
+        // this.repoUrl = 'https://github.com/ymoan/normal-character.git' //github源库【优先更新】
         this.profilePath = path.join(process.cwd(), 'plugins', 'miao-plugin', 'resources', 'profile')
         this.repoPath = path.join(this.profilePath, 'normal-character')
     }
